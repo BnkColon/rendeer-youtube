2f314265834a8e2c8f21ab0adc6c6dbcc29485de
Bianca Colon <colon.bianca@gmail.com>
2017-05-08 15:39:02 -0400

    Function parrot_trouble added.

--- Python-Challenges/challenges.py
+++ Python-Challenges/challenges.py
@@ -47,7 +47,6 @@ def sum_double(a, b):
   return result
 
 # ---------------------------------------------------------------------------
-
 # Given an int n, return the absolute difference between n and 21, except 
 # return double the absolute difference if n is over 21.
 
@@ -61,3 +60,20 @@ def diff21(n):
   else:
     result = (21-n)
   return result
+
+# ---------------------------------------------------------------------------
+# We have a loud talking parrot. The "hour" parameter is the current hour time 
+# in the range 0..23. We are in trouble if the parrot is talking and the hour 
+# is before 7 or after 20. Return True if we are in trouble.
+
+# parrot_trouble(True, 6) → True
+# parrot_trouble(True, 7) → False
+# parrot_trouble(False, 6) → False
+
+def parrot_trouble(talking, hour):
+  if (talking==True and (hour< 7 or hour > 20)):
+    return True
+  else:
+    return False
+
+# ---------------------------------------------------------------------------
